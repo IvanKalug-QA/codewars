2a373e77992409382228c72e5e3941219a7f9448
IvanKalug-QA <kaluginivan2002@mail.ru>
2023-07-09 13:58:49 +0300

    Added task

---
+++ Double_Char.py
@@ -0,0 +1,15 @@
+# DESCRIPTION:
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+#
+# Examples (Input -> Output):
+# * "String"      -> "SSttrriinngg"
+# * "Hello World" -> "HHeelllloo  WWoorrlldd"
+# * "1234!_ "     -> "11223344!!__  "
+# Good Luck!
+
+def double_char(s):
+    n = ''
+    for i in s:
+        i *= 2
+        n += i
+    return n
