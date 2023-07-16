9c7f1f075a9ef6e7dc1fd265232f3ae6735dccee
IvanKalug-QA <kaluginivan2002@mail.ru>
2023-07-16 13:37:59 +0300

    Added task

---
+++ Enumerable_Magic_-_Does_My_List_Include_This.py
@@ -0,0 +1,8 @@
+# DESCRIPTION:
+# Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+def include(arr,item):
+    for i in arr:
+        if i == item:
+            return True
+    return False
