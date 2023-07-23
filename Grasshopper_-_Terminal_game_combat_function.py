6ebdc41b8e091198e707916bdb37533e728e63c7
IvanKalug-QA <kaluginivan2002@mail.ru>
2023-07-23 18:17:14 +0300

    Added task

---
+++ Grasshopper_-_Terminal_game_combat_function.py
@@ -0,0 +1,8 @@
+# DESCRIPTION:
+# Create a combat function that takes the player's current health and the amount of damage recieved, '
+# 'and returns the player's new health. Health can't be less than 0.
+
+def combat(health, damage):
+    if health - damage < 0:
+        return 0
+    return health - damage
