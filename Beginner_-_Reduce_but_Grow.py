c64287f40257aabb63159b5e6d9eb9c83b2cd7fc
IvanKalug-QA <kaluginivan2002@mail.ru>
2023-08-11 22:06:27 +0300

    Added task

---
+++ Beginner_-_Reduce_but_Grow.py
@@ -0,0 +1,11 @@
+# DESCRIPTION:
+# Given a non-empty array of integers,
+# return the result of multiplying the values together in order. Example:
+#
+# [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+def grow(arr):
+    s = 1
+    for i in arr:
+        s *= i
+    return s
