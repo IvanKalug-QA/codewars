0751f87c36735d082575d3ba2fc47eb38b704f10
IvanKalug-QA <kaluginivan2002@mail.ru>
2023-08-25 21:22:27 +0300

    Added task

---
+++ Exclamation_marks_series_6_Remove_n_exclamation_marks_in_the_sentence_from_left_to_right.py
@@ -0,0 +1,23 @@
+# DESCRIPTION:
+# Description:
+# Remove n exclamation marks in the sentence from left to right. n is positive integer.
+#
+# Examples
+# remove("Hi!",1) === "Hi"
+# remove("Hi!",100) === "Hi"
+# remove("Hi!!!",1) === "Hi!!"
+# remove("Hi!!!",100) === "Hi"
+# remove("!Hi",1) === "Hi"
+# remove("!Hi!",1) === "Hi!"
+# remove("!Hi!",100) === "Hi"
+# remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+# remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+# remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+# remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+def remove(s, n):
+    count = 0
+    while count < n:
+        s = s.replace('!','',1)
+        count += 1
+    return s
