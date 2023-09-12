545df79f2daf0efd432b5dd1fdfee45aea976fb7
IvanKalug-QA <kaluginivan2002@mail.ru>
2023-09-12 19:54:47 +0300

    Added task

---
+++ Moving_Zeros_To_The_End.py
@@ -0,0 +1,15 @@
+# DESCRIPTION:
+# Write an algorithm that takes an array and moves all of the zeros to the end,
+# preserving the order of the other elements.
+#
+# move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+
+def move_zeros(lst):
+    l = []
+    l1 = []
+    for i in lst:
+        if i > 0:
+            l.append(i)
+        if i == 0:
+            l1.append(i)
+    return l + l1
