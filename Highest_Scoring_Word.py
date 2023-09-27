2be290b085e5ee1acb2c7e0feb459d89a869d1a9
IVAN-GUY <kaluginivan2002@mail.ru>
2023-09-27 22:18:50 +0300

    Added task

---
+++ Highest_Scoring_Word.py
@@ -0,0 +1,37 @@
+# DESCRIPTION:
+# Given a string of words, you need to find the highest scoring word.
+#
+# Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+#
+# For example, the score of abad is 8 (1 + 2 + 1 + 4).
+#
+# You need to return the highest scoring word as a string.
+#
+# If two words score the same, return the word that appears earliest in the original string.
+#
+# All letters will be lowercase and all inputs will be valid.
+
+def high(x):
+    count = 0
+    dict = {'a': 1, 'b': 2, 'c': 3,
+            'd': 4, 'e': 5, 'f': 6,
+            'g': 7, 'h': 8, 'i': 9,
+            'j': 10, 'k': 11, 'l': 12,
+            'm': 13, 'n': 14, 'o': 15,
+            'p': 16, 'q': 17, 'r': 18,
+            's': 19, 't': 20, 'u': 21,
+            'v': 22, 'w': 23, 'x': 24,
+            'y': 25, 'z': 26
+    }
+    dict_words = {}
+    for i in x.split():
+        s = i
+        for i in s:
+            if i in dict:
+                count += dict[i]
+        dict_words[s] = count
+        count = 0
+    ma = max(dict_words.values())
+    for k in dict_words.keys():
+        if dict_words[k] == ma:
+            return k
