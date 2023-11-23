7f16d0ad64c60741218773e1acfb4dc7fe4a19ed
IVAN-GUY <kaluginivan2002@mail.ru>
2023-11-23 17:50:53 +0300

    Added task

---
+++ Penultimate.py
@@ -0,0 +1,11 @@
+# Find the second-to-last element of a list.
+#
+# The input list will always contain at least two elements.
+#
+# Example:
+#
+# penultimate([1,2,3,4])            # => 3
+# penultimate("Python is dynamic") # => 'i'
+
+def penultimate(a):
+    return a[-2]
