ced0c0e11df3e5499d3a0ef18ae3fcbca61e53da
IVAN-GUY <kaluginivan2002@mail.ru>
2023-12-26 17:43:12 +0300

    Added task

---
+++ Number_Pairs.py
@@ -0,0 +1,18 @@
+# In this Kata the aim is to compare each pair of integers from 2 arrays, and return a new array of large numbers.
+#
+# Note: Both arrays have the same dimensions.
+#
+# Example:
+#
+# arr1 = [13, 64, 15, 17, 88]
+# arr2 = [23, 14, 53, 17, 80]
+# get_larger_numbers(arr1, arr2) == [23, 64, 53, 17, 88]
+
+def get_larger_numbers(a, b):
+    l = []
+    for i in range(len(a)):
+        if a[i] > b[i] or a[i] == b[i]:
+            l.append(a[i])
+        elif b[i] > a[i]:
+            l.append(b[i])
+    return l
