3a2d41882ea984f4dc693977c3374988a1f2256b
IVAN-GUY <kaluginivan2002@mail.ru>
2024-01-07 11:05:46 +0300

    Added task

---
+++ Vowel_one.py
@@ -0,0 +1,20 @@
+# vowelOne
+# Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+#
+# All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+#
+# Examples:
+#
+# vowelOne "abceios" -- "1001110"
+#
+# vowelOne "aeiou, abc" -- "1111100100"
+
+def vowel_one(s):
+    word = ['a', 'o', 'e', 'i', 'u']
+    st = ''
+    for i in s.lower():
+        if i in word:
+            st += '1'
+        else:
+            st += '0'
+    return st
