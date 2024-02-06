50931d278a173436ee2b962d38199f3c0f2da14a
IVAN-GUY <kaluginivan2002@mail.ru>
2024-02-06 06:15:13 +0300

    Added task

---
+++ getNames().py
@@ -0,0 +1,17 @@
+# The following code is not giving the expected results. Can you debug what the issue is?
+#
+# The following is an example of data that would be passed in to the function.
+#
+# data = [
+#     {'name': 'Joe', 'age': 20},
+#     {'name': 'Bill', 'age': 30},
+#     {'name': 'Kate', 'age': 23}
+# ]
+# get_names(data) # should return ['Joe', 'Bill', 'Kate']
+
+def itemgetter(item):
+    item['name']
+
+
+def get_names(data):
+    return [i['name'] for i in data]
