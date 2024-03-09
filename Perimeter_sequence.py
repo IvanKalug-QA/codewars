44584db4a09753a840c0751348c72ef1dfe38794
IVAN-GUY <kaluginivan2002@mail.ru>
2024-03-09 09:41:11 +0300

    Added task

---
+++ Perimeter_sequence.py
@@ -0,0 +1,11 @@
+# Perimeter sequence
+# The first three stages of a sequence are shown.
+#
+# blocks
+#
+# The blocksize is a by a and a ≥ 1.
+#
+# What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+
+def perimeter_sequence(a, n):
+    return 4*n*a
