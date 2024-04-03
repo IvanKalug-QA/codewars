50130d9a1467b99f1d686eccbb35e35be76ee74d
IVAN-GUY <kaluginivan2002@mail.ru>
2024-04-03 06:21:24 +0300

    Added task

---
+++ Regexp_Basics_is_it_a_vowel.py
@@ -0,0 +1,4 @@
+# Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
+
+def is_vowel(s):
+    return s.lower() in ['a', 'e','i', 'o', 'u']
