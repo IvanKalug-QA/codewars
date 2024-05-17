dd2aec88e8da3e5f944161f016ee6374d83327fa
IVAN-GUY <kaluginivan2002@mail.ru>
2024-05-17 06:27:24 +0300

    Added task

---
+++ Show_multiples_of_2_numbers_within_a_range.py
@@ -0,0 +1,11 @@
+# Print all numbers up to 3rd parameter which are multiple of both 1st and 2nd parameter.
+#
+# Python, Javascript, Java, Ruby versions: return results in a list/array
+#
+# NOTICE:
+#
+# Do NOT worry about checking zeros or negative values.
+# To find out if 3rd parameter (the upper limit) is inclusive or not, check the tests, it differs in each translation
+
+def multiples(s1,s2,s3):
+    return [i for i in range(1, s3) if i % s1 == 0 and i % s2 == 0]
