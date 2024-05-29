c0c5c740a6b001d87e1219767e0bfb31eae70d07
IVAN-GUY <kaluginivan2002@mail.ru>
2024-05-29 06:44:46 +0300

    Added task

---
+++ Coding_3min_Bug_in_Apple.py
@@ -0,0 +1,16 @@
+# This is the simple version of Shortest Code series. If you need some challenges, please try the challenge version.
+#
+# Task:
+# Find out "B"(Bug) in a lot of "A"(Apple).
+#
+# There will always be one bug in apple, not need to consider the situation that without bug or more than one bugs.
+#
+# input: string Array apple
+#
+# output: Location of "B", [x,y]
+
+def sc(apple):
+    for k, v in enumerate(apple):
+        for j, c in enumerate(v):
+            if c == 'B':
+                return [k, j]
