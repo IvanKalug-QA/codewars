55fb45054b6dde015bef0a22be7379ac428fd6d9
IVAN-GUY <kaluginivan2002@mail.ru>
2024-10-03 06:38:32 +0300

    Added task

---
+++ Find_The_Duplicated_Number_in_a_Consecutive_Unsorted_List_Tougher_Version.py
@@ -0,0 +1,11 @@
+# Spin-off of this kata, here you will have to figure out an efficient strategy to solve the problem of finding the sole duplicate number among an unsorted array/list of numbers starting from 1 up to n.
+#
+# Hints: a solution in linear time can be found; using the most intuitive ones to search for duplicates that can run in O(n²) time won't work.
+
+def find_dup(arr):
+    d = {}
+    for i in arr:
+        d[i] = d.get(i, 0) + 1
+    for i in d:
+        if d[i] >= 2:
+            return i
