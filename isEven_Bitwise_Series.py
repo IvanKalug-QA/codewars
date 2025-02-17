62568fd555f8b1b12933a7685a7211513a1dc99b
IVAN-GUY <kaluginivan2002@mail.ru>
2025-02-17 06:28:38 +0300

    Added task

---
+++ isEven_Bitwise_Series.py
@@ -0,0 +1,15 @@
+# Is the number even?
+# If the numbers is even return true. If it's odd, return false.
+#
+#
+#
+#
+#
+# Oh yeah... the following symbols/commands have been disabled!
+#
+# use of %
+# use of .even? in Ruby
+# use of mod in Python
+
+def is_even(n):
+    return n & 1 == 0
