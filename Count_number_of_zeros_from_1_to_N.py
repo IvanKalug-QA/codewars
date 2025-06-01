8f95bf153b2f641d9f5d7e70834db11d8de8247f
IVAN-GUY <kaluginivan2002@mail.ru>
2025-06-01 05:56:47 +0300

    Added task

---
+++ Count_number_of_zeros_from_1_to_N.py
@@ -0,0 +1,10 @@
+# Create an algorithm to count the number of zeros that appear between 1 and N.
+#
+# Examples
+#  10  -->   1  // 10
+#  20  -->   2  // 10, 20
+# 100  -->  11  // 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+# 200  -->  31
+
+def count_zeros(x):
+    return sum(str(i).count('0') for i in range(1, x + 1))
