7fee4a7b0f60b063e1b27e6da39af868c9349695
IVAN-GUY <kaluginivan2002@mail.ru>
2025-08-10 07:25:40 +0300

    Added task

---
+++ Player_Contact_Manager.py
@@ -0,0 +1,26 @@
+# You are the Dungeon Master for a public DnD game at your local comic shop and recently you've had some trouble keeping your players' info neat and organized so you've decided to write a bit of code to help keep them sorted!
+#
+# The goal of this code is to create an array of objects that stores a player's name and contact number from a given string.
+#
+# The method should return an empty array if the argument passed is an empty string or nil/None/null.
+#
+# Examples
+# player_manager("John Doe, 8167238327, Jane Doe, 8163723827") returns [{"player": "John Doe", "contact": 8167238327}, {"player": "Jane Doe", "contact": 8163723827}]
+# player_manager(None) returns []
+# player_manager("")   returns []
+# playerManager("John Doe, 8167238327, Jane Doe, 8163723827") returns [{player: "John Doe", contact: 8167238327}, {player: "Jane Doe", contact: 8163723827}]
+# playerManager(null) returns []
+# playerManager("")   returns []
+# Have at thee!
+
+from collections import deque
+def player_manager(players):
+    if not isinstance(players, str) or not players:
+        return []
+    contact = []
+    d = deque(players.split(','))
+    while d:
+        p = d.popleft().strip()
+        c = d.popleft().strip()
+        contact.append({'player': p, 'contact': int(c)})
+    return contact
