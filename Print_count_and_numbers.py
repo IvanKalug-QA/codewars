91a9eccaee2987474b1d045699a8fa57d748c3e4
IVAN-GUY <kaluginivan2002@mail.ru>
2025-09-08 08:41:11 +0300

    Added task

---
+++ Print_count_and_numbers.py
@@ -0,0 +1,34 @@
+# Given a string of integers, count how many times that integer repeats itself, then return a string showing the count and the integer.
+#
+# Example: "1123"
+#
+# Here 1 comes twice so <count><integer> will be "21"
+# then 2 comes once so <count><integer> will be "12"
+# then 3 comes once so <count><integer> will be "13"
+# hence output string will be "211213".
+#
+# Similarly "211213" will return "1221121113" (1 time 2, 2 times 1, 1 time 2, 1 time 1, 1 time 3)
+#
+# Return "" for empty, nil or non numeric strings
+
+
+def count_me(data):
+    if data is None or data == "":
+        return ""
+
+    if not data.isdigit():
+        return ""
+
+    result = []
+    i = 0
+    n = len(data)
+
+    while i < n:
+        count = 1
+        while i + 1 < n and data[i] == data[i + 1]:
+            count += 1
+            i += 1
+        result.append(str(count) + data[i])
+        i += 1
+
+    return "".join(result)
