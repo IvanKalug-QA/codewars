34c671a749a911d2a3fc0ab41880f9cbebcd13db
IVAN-GUY <kaluginivan2002@mail.ru>
2025-12-26 08:20:07 +0300

    added task

---
+++ Find_the_area_of_the_rectangle.py
@@ -0,0 +1,8 @@
+# Find the area of a rectangle when provided with one diagonal and one side of the rectangle. If the input diagonal is less than or equal to the length of the side, return "Not a rectangle". If the resultant area has decimals round it to two places.
+
+def area(d, l): 
+    if d <= l:
+        return "Not a rectangle"
+    w = (d ** 2 - l ** 2) ** 0.5
+    s = l * w
+    return round(s, 2)
