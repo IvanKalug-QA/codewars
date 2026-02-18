84b0676aa00d0201d3481f6b659cdd187e3eba30
IVAN-GUY <kaluginivan2002@mail.ru>
2026-02-18 08:17:06 +0300

    added task

---
+++ Weight_of_its_Contents.py
@@ -0,0 +1,25 @@
+# Welcome to the Mathematics gameshow. I'm your host, Apex Rhombus, and it's time for the lightning round!
+
+# Today we'll talk about a hypothetical bottle. This entire bottle weighs 120 grams. Its contents weigh twice as much as the bottle itself. What, may I ask, do the contents weigh?
+
+# ...Did you guess 80 grams? Correct! Now that you've got that idea, I'm gonna ask you that question in many different ways so you'd better get ready!
+
+# Let's complete the function that takes in two parameters: bottle weight and scale. This function will return the weight of the contents inside the bottle.
+
+# bottle weight will be an integer representing the weight of the entire bottle (contents included).
+
+# scale will be a string that you will need to parse. It will tell you how the content weight compares to the weight of the bottle by itself. 2 times larger, 6 times larger, and 15 times smaller would all be valid strings (smaller and larger are the only comparison words).
+
+# It is guaranteed that the result will always be an integer. Good luck!
+
+
+def content_weight(bottle_weight, scale): 
+    parts = scale.split()
+    times = int(parts[0])
+    comparison = parts[2]
+    
+    if comparison == "larger":
+        bottle = bottle_weight / (times + 1)
+        return bottle * times
+    else:
+        return bottle_weight / (times + 1)
