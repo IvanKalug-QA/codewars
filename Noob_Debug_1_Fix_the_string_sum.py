217bde067c49abbef5e617cd0de92c9641068466
IVAN-GUY <kaluginivan2002@mail.ru>
2026-03-02 08:09:57 +0300

    added task

---
+++ Noob_Debug_1_Fix_the_string_sum.py
@@ -0,0 +1,7 @@
+# Help Johnny!
+# He can't make his code work!
+# Easy Code
+# Johnny is trying to make a function that encode 2 strings into their ASCII values and sum them together, but he can't find the error in his code! Help him!
+
+def add(s1, s2):
+    return sum(ord(i) for i in s1 + s2)
