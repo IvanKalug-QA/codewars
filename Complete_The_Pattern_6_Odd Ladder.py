6f79b7ec13dd858311c8f475d604ffd9bc0f9dce
IVAN-GUY <kaluginivan2002@mail.ru>
2026-03-29 09:31:42 +0300

    added task

---
+++ Complete_The_Pattern_6_Odd Ladder.py
@@ -0,0 +1,30 @@
+# Task:
+# You have to write a function which creates the following odd-numbers staircase pattern up to the desired number of rows.
+
+# If the input is <= 0, return an empty string
+# If the input is even, round it down to the odd number that precedes it.
+# There are no spaces in the pattern
+# The lines are separated by a newline character (\n)
+# Examples:
+# pattern(9):
+
+# 1
+# 333
+# 55555
+# 7777777
+# 999999999
+# pattern(6):
+
+# 1
+# 333
+# 55555
+
+def pattern(n: int) -> str:
+    if n <= 0:
+        return ''
+    if n % 2 == 0:
+        n -= 1
+    result = []
+    for i in range(1, n + 1, 2):
+        result.append(str(i) * i)
+    return '\n'.join(result)
