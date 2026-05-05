ef2fb98f013b96443147e105430dd99ef4e5815d
IVAN-GUY <kaluginivan2002@mail.ru>
2026-05-05 08:15:33 +0300

    added task

---
+++ Get_Zodiac_Sign.py
@@ -0,0 +1,50 @@
+# Your task is to get Zodiac Sign using input day and month.
+
+# For example:
+
+# get_zodiac_sign(1,5) => 'Taurus'
+# get_zodiac_sign(10,10) => 'Libra'
+# Correct answers are (preloaded):
+
+# SIGNS = ['Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius']
+# P.S. Each argument is correct integer number.
+
+# WESTERN ASTROLOGY STAR SIGN DATES
+
+# Aries (March 21-April 19)
+# Taurus (April 20-May 20)
+# Gemini (May 21-June 20)
+# Cancer (June 21-July 22)
+# Leo (July 23-August 22)
+# Virgo (August 23-September 22)
+# Libra (September 23-October 22)
+# Scorpio (October 23-November 21)
+# Sagittarius (November 22-December 21)
+# Capricorn (December 22-January 19)
+# Aquarius (January 20 to February 18)
+# Pisces (February 19 to March 20)
+
+def get_zodiac_sign(day, month):
+    if (month == 1 and day >= 20) or (month == 2 and day <= 18):
+        return 'Aquarius'
+    if (month == 2 and day >= 19) or (month == 3 and day <= 20):
+        return 'Pisces'
+    if (month == 3 and day >= 21) or (month == 4 and day <= 19):
+        return 'Aries'
+    if (month == 4 and day >= 20) or (month == 5 and day <= 20):
+        return 'Taurus'
+    if (month == 5 and day >= 21) or (month == 6 and day <= 20):
+        return 'Gemini'
+    if (month == 6 and day >= 21) or (month == 7 and day <= 22):
+        return 'Cancer'
+    if (month == 7 and day >= 23) or (month == 8 and day <= 22):
+        return 'Leo'
+    if (month == 8 and day >= 23) or (month == 9 and day <= 22):
+        return 'Virgo'
+    if (month == 9 and day >= 23) or (month == 10 and day <= 22):
+        return 'Libra'
+    if (month == 10 and day >= 23) or (month == 11 and day <= 21):
+        return 'Scorpio'
+    if (month == 11 and day >= 22) or (month == 12 and day <= 21):
+        return 'Sagittarius'
+    return 'Capricorn'
