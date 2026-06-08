f3918b5c2f30f90b7d298729fdf7cb152e6905b5
IVAN-GUY <kaluginivan2002@mail.ru>
2026-06-08 08:16:59 +0300

    added task

---
+++ You_Complete_Me.py
@@ -0,0 +1,35 @@
+# Task: concatenate one or more letters to the end of a given input string to create a string that reads the same forward as backward.
+
+# Specifications
+
+# Letters can only be added to the right hand side of the input string.
+
+# You are required to add at least one letter to the input string.
+
+# The shorter return string is the correct one. ie.. (ab)=> abba is Wrong, while (ab)=> aba is Right.
+
+# Inputs will consist of one or more letters, upper and lower case.
+
+# Strings are case sensitive. ie..(Gn)=> gng is Wrong, while (Gn)=> GnG is right.
+
+# Examples:
+
+# (a) => a + a => aa
+
+# (GG) => GG + G => GGG
+
+# (Ab) => Ab + A => AbA
+
+# (aba) => aba + ba => ababa
+
+# (aab) => aab + aa => aabaa
+
+
+
+def complete(st):
+    n = len(st)
+    for i in range(1, n + 1):
+        suffix = st[i:]
+        if suffix == suffix[::-1]:
+            return st + st[:i][::-1]
+    return st + st[:-1][::-1]
