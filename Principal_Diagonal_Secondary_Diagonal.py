f075ea95556d49e78f58ffc9e3e20e385f12e179
IVAN-GUY <kaluginivan2002@mail.ru>
2026-06-24 08:17:09 +0300

    added task

---
+++ Principal_Diagonal_Secondary_Diagonal.py
@@ -0,0 +1,35 @@
+# Principal Diagonal -- The principal diagonal in a matrix identifies those elements of the matrix running from North-West to South-East.
+
+# Secondary Diagonal -- the secondary diagonal of a matrix identifies those elements of the matrix running from North-East to South-West.
+
+# For example:
+
+# matrix:             [1, 2, 3]
+#                     [4, 5, 6]
+#                     [7, 8, 9]
+
+# principal diagonal: [1, 5, 9]
+# secondary diagonal: [3, 5, 7]
+# Task
+# Your task is to find which diagonal is "larger": which diagonal has a bigger sum of their elements.
+
+# If the principal diagonal is larger, return "Principal Diagonal win!"
+# If the secondary diagonal is larger, return "Secondary Diagonal win!"
+# If they are equal, return "Draw!"
+# Note: You will always receive matrices of the same dimension.
+
+def diagonal(matrix):
+    n = len(matrix)
+    principal_sum = 0
+    secondary_sum = 0
+    
+    for i in range(n):
+        principal_sum += matrix[i][i]
+        secondary_sum += matrix[i][n - 1 - i]
+    
+    if principal_sum > secondary_sum:
+        return "Principal Diagonal win!"
+    elif secondary_sum > principal_sum:
+        return "Secondary Diagonal win!"
+    else:
+        return "Draw!"
