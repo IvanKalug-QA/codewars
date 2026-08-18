bc04c537fb4d67bd6571e6dbb0ee9014f63c76ad
IVAN-GUY <kaluginivan2002@mail.ru>
2026-08-18 08:13:42 +0300

    added task

---
+++ Satisfying_numbers.py
@@ -0,0 +1,23 @@
+# 2520 is the smallest number that can be divided by each of the integers from 1 to 10 without any remainder.
+
+# Find the smallest positive number that is evenly divisible by all of the integers from 1 to n, (in this kata, 
+# 1
+# ≤
+# n
+# ≤
+# 40
+# 1≤n≤40).
+
+# For example:
+
+# 5 --> 60      // 1 to 5 can all divide evenly into 60
+# 10 --> 2520
+
+
+from math import gcd
+
+def smallest(n):
+    lcm = 1
+    for i in range(1, n + 1):
+        lcm = lcm * i // gcd(lcm, i)
+    return lcm
